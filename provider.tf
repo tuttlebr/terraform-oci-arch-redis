86db8bc9bf1521b3957e6bb9bc12435da4a59d2d
Brandon Tuttle <915120+tuttlebr@users.noreply.github.com>
2023-03-27 11:39:21 -0400

    Update region to declared variable

--- provider.tf
+++ provider.tf
@@ -24,7 +24,7 @@ provider "oci" {
   user_ocid            = var.user_ocid
   fingerprint          = var.fingerprint
   private_key_path     = var.private_key_path
-  region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
+  region               = var.region
   disable_auto_retries = "true"
 }
 
